6b93fd7dc705914e11e0fe980abb95e43c8812a5
Oliver Reifler <oliver.reifler@itvitae.nl>
2024-03-21 23:20:00 +0100

    Added RecipeListDto

---
+++ RecipeBook.Domain/Dtos/RecipeListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeBook.Domain.Dtos
+{
+    public class RecipeListDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string[] Images { get; set; } = [];
+    }
+}
